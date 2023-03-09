c9663ab2f6eb478c55ab2c5d96b851ee886743d5
Ponti-D <spaediaz@gmail.com>
2023-03-09 18:27:44 +0000

    feat: Implement GET request and test for coffee lover

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
-import express from 'express';
+import express from "express";
 export const app = express();
 
-app.get('/', (req, res) => res.send('Welcome to the Drinks API!'));
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("Welcome to the Drinks API!"));
+
+app.get("/coffeelover", (req, res) => res.send("I like coffee!"));
 
