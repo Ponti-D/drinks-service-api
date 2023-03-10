37af4fe32c714a61d115a8468e727119ac81a870
Ponti-D <spaediaz@gmail.com>
2023-03-10 20:54:17 +0000

    Implemented running as a full server

--- src/controllers/coffee_request.test.ts
+++ src/controllers/coffee_request.test.ts
@@ -1,6 +1,7 @@
 import request from "supertest";
 import { app } from "../app";
 
+
 describe("Test /coffee API endpoint request", () => {
   test("GET should return correct JSON object", async () => {
     const res = await request(app)
